61478e500d599467fe302138ef519701e2766b90
evy.heynen <evy.heynen@telenet.be>
2018-03-16 17:01:55 +0100

    webtest 1 running

--- src/test/java/be/qnh/contact/Webtest.java
+++ src/test/java/be/qnh/contact/Webtest.java
@@ -1,66 +1,79 @@
 package be.qnh.contact;
 
+import be.qnh.contact.Model.Address;
+import be.qnh.contact.Model.ContactPage;
+import be.qnh.contact.Model.Person;
+import be.qnh.contact.Model.Subject;
 import be.qnh.contact.controller.ContactController;
 import be.qnh.contact.service.ContactService;
 import org.hamcrest.Matchers;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(ContactController.class)
 public class Webtest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private ContactService contactService;
-
-    @InjectMocks
-    private ContactController contactController;
-
-    @Before
-    public void intit(){
-        mockMvc=MockMvcBuilders.standaloneSetup(contactController).build();
-    }
 
     @Test
     public void findAllContactPagesTest() throws Exception{
+        List<ContactPage> contactPageList = new ArrayList<>();
+        ContactPage contactPage = new ContactPage();
+        Person person1 = new Person();
+        Address addressEvy = new Address();
+        Subject subject = Subject.JAVA;
+
+        List<Address> addressList = new ArrayList<>();
+        addressList.add(addressEvy);
+
+        addressEvy.setStreet("Heidestraat");
+        addressEvy.setNumber("52D");
+        addressEvy.setPostalCode("3945");
+        addressEvy.setTown("Ham");
+
+        person1.setFirstName("Evy");
+        person1.setLastName("Heynen");
+        person1.setAddress(addressList);
+        person1.setMail("evy.heynen@telenet.be");
+
+        contactPage.setPerson(person1);
+        contactPage.setSubject(subject);
+        contactPage.setQuestion("How to download JAVA?");
+
+        contactPageList.add(contactPage);
+
+        given(contactService.findAllContactPages()).
+                willReturn(contactPageList);
+
         mockMvc.perform(get("/contactpage")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.*", Matchers.hasSize(0)));
+                .andExpect(jsonPath("$.*", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].question", Matchers.is("How to download JAVA?")))
+                .andExpect(jsonPath("$[0].subject",Matchers.is("JAVA")));
+//                .andExpect(jsonPath("$[0].person[0].firstName",Matchers.is("Evy")));
+
+
 
     }
 
-//    ContactPage contactPage=new ContactPage();
-//    Person person1=new Person();
-//    Address addressEvy=new Address();
-//    Subject subject=Subject.JAVA;
-//
-//    List<Address> addressList =new ArrayList<>();
-//        addressList.add(addressEvy);
-//
-//        addressEvy.setStreet("Heidestraat");
-//        addressEvy.setNumber("52D");
-//        addressEvy.setPostalCode("3945");
-//        addressEvy.setTown("Ham");
-//
-//        person1.setFirstName("Evy");
-//        person1.setLastName("Heynen");
-//        person1.setAddress(addressList);
-//        person1.setMail("evy.heynen@telenet.be");
-//
-//        contactPage.setPerson(person1);
-//        contactPage.setSubject(subject);
-//        contactPage.setQuestion("How to download JAVA?");
 }
